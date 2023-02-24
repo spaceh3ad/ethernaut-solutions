56f33f28db0cf30daf7ed50ad1cdc3b07b22a2b2
spaceh3ad <jan.kwiatkowski.web3@gmail.com>
2023-02-24 20:03:53 +0100

    20th done

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -20,7 +20,7 @@ const config: HardhatUserConfig = {
   defaultNetwork: "goerli",
   networks: {
     goerli: {
-      url: `https://goerli.infura.io/v3/${process.env.INFURA_KEY}`,
+      url: `https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_KEY}`,
       accounts: [process.env.PRIVATE_KEY!!],
       gas: 1_000_000,
     },
